197b97fefac46068a2f4ab82884e9c5e018a0e27
Dan Grover <dan@dangrover.com>
2025-05-18 13:02:27 -0700

    first try at the task

--- csc450ct5.cpp
+++ csc450ct5.cpp
@@ -1,11 +1,6 @@
-/*
-Program: Create a C++ program that will obtain input from a user and store it into the provided CSC450_CT5_mod5.txt Download CSC450_CT5_mod5.txtfile. 
-Download CSC450_CT5_mod5.txt Download CSC450_CT5_mod5.txtfile. Your program should append it to the provided text file, without deleting the existing data: 
-Store the provided data in the CSC450_CT5_mod5.txt file.
-Create a reversal method that will reverse all of the characters in the CSC450_CT5_mod5.txt file and store the result in a CSC450-mod5-reverse.txt file.
-
-*/
-
+// Project for CSC450
+// Append user input to a file, and then, for some reason, 
+// write a reversed copy of the file to a new file.
 
 #include <iostream>
 #include <fstream>
